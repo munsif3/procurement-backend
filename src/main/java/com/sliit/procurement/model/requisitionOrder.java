65d2682edad0a3d02d1ebcb57e4902857a8be170
KiruthigaBalendran <kiruthiga03@gmail.com>
2017-11-20 00:48:15 +0530

    models for requisitionOrder and requisitionOrderDetails

---
+++ src/main/java/com/sliit/procurement/model/requisitionOrder.java
@@ -0,0 +1,111 @@
+package com.sliit.procurement.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+/**
+ * Created by User on 20/11/2017.
+ */
+
+@Entity
+@Table(name="requisition_order")
+public class requisitionOrder {
+
+    @Id
+    @Column(name="requisitionNo")
+    private int requisitionNo;
+
+    @Column(name="requisitionId")
+    private String requisitionId;
+
+    @Column(name="orderBy")
+    private int orderBy;
+
+    @Column(name="reqOrderedDate")
+    @JsonFormat(pattern="yyyy-MM-dd")
+    private Date reqOrderedDate;
+
+    @Column(name="reqExpectedDate")
+    @JsonFormat(pattern="yyyy-MM-dd")
+    private Date reqExpectedDate;
+
+    @Column(name="status")
+    private String status;
+
+    @Column(name="comments")
+    private String comments;
+
+    public requisitionOrder() {
+    }
+
+    public requisitionOrder(int requisitionNo, String requisitionId, int orderBy, Date reqOrderedDate, Date reqExpectedDate, String status, String comments) {
+        this.requisitionNo = requisitionNo;
+        this.requisitionId = requisitionId;
+        this.orderBy = orderBy;
+        this.reqOrderedDate = reqOrderedDate;
+        this.reqExpectedDate = reqExpectedDate;
+        this.status = status;
+        this.comments = comments;
+    }
+
+    public int getRequisitionNo() {
+        return requisitionNo;
+    }
+
+    public void setRequisitionNo(int requisitionNo) {
+        this.requisitionNo = requisitionNo;
+    }
+
+    public String getRequisitionId() {
+        return requisitionId;
+    }
+
+    public void setRequisitionId(String requisitionId) {
+        this.requisitionId = requisitionId;
+    }
+
+    public int getOrderBy() {
+        return orderBy;
+    }
+
+    public void setOrderBy(int orderBy) {
+        this.orderBy = orderBy;
+    }
+
+    public Date getReqOrderedDate() {
+        return reqOrderedDate;
+    }
+
+    public void setReqOrderedDate(Date reqOrderedDate) {
+        this.reqOrderedDate = reqOrderedDate;
+    }
+
+    public Date getReqExpectedDate() {
+        return reqExpectedDate;
+    }
+
+    public void setReqExpectedDate(Date reqExpectedDate) {
+        this.reqExpectedDate = reqExpectedDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+}
---
+++ src/main/java/com/sliit/procurement/model/requisitionOrderDetails.java
@@ -0,0 +1,72 @@
+package com.sliit.procurement.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+/**
+ * Created by User on 20/11/2017.
+ */
+
+@Entity
+@Table(name="requisition_order_details")
+public class requisitionOrderDetails {
+
+    @Id
+    @Column(name="requisitionNo")
+    private int requisitionNo;
+
+    @Id
+    @Column(name="itemId")
+    private int itemId;
+
+    @Column(name="quantity")
+    private Double quantity;
+
+    @Column(name="quantityUnit")
+    private String quantityUnit;
+
+    public requisitionOrderDetails() {
+    }
+
+    public requisitionOrderDetails(int requisitionNo, int itemId, Double quantity, String quantityUnit) {
+        this.requisitionNo = requisitionNo;
+        this.itemId = itemId;
+        this.quantity = quantity;
+        this.quantityUnit = quantityUnit;
+    }
+
+    public int getRequisitionNo() {
+        return requisitionNo;
+    }
+
+    public void setRequisitionNo(int requisitionNo) {
+        this.requisitionNo = requisitionNo;
+    }
+
+    public int getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(int itemId) {
+        this.itemId = itemId;
+    }
+
+    public Double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Double quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getQuantityUnit() {
+        return quantityUnit;
+    }
+
+    public void setQuantityUnit(String quantityUnit) {
+        this.quantityUnit = quantityUnit;
+    }
+}
