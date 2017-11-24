0b07ccb575e706a9109d24ebbb182a4f96428e55
Sudheesan <surendransudheesan@gmail.com>
2017-11-25 01:47:48 +0530

    new test class added

---
+++ src/test/java/com/sliit/DepartmentControllerTest.java
@@ -0,0 +1,24 @@
+package com.sliit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DepartmentControllerTest {
+
+	@Test
+	public void testListAllUsers() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateNote() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateDepartment() {
+		fail("Not yet implemented");
+	}
+
+}
